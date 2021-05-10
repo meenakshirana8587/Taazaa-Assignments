41aeed045d5f8632be1ad22cda3b67bd07970563
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-05-10 19:02:32 +0530

    Sum Consecutives-Kata-4:(10/5/2021)

---
+++ SumConsecutives/SumConsecutives/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumConsecutives
+{
+    public class Data
+    {
+        public static List<int> data = new List<int> {1,4,4,4,0,4,2,2,1 };
+
+    }
+}
---
+++ SumConsecutives/SumConsecutives/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumConsecutives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(); 
+            list = Data.data;
+            Operation oper = new Operation();
+            
+           oper.SumConsecutives(list).ForEach(i=> { Console.WriteLine(i); });
+        }
+    }
+}
