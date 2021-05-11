45387e78b8eef3d823eb3b82ec10d8bdeebcbdde
meenakshirana8587 <69763066+meenakshirana8587@users.noreply.github.com>
2021-05-11 11:50:43 +0530

    sumConsecutives:Kata-4(10/5/2021)

---
+++ SumConsecutives/SumConsecutives/Operation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumConsecutives
+{
+    public class Operation
+    {
+       public static  List<int> resultList = new List<int>();
+        public List<int> SumConsecutives(List<int> list)
+        {
+            
+            int i = 0;
+            int sum = 0;
+            //Console.WriteLine(list.Count);
+            while (i < list.Count-1)
+            {
+                
+
+                if (list[i] == list[i + 1])
+                {
+                    sum = sum + list[i];
+
+                }
+                else
+                {
+                    resultList.Add(sum+list[i]);
+                    sum = 0;
+                }
+                
+               
+                
+                i++;
+
+            }
+            return resultList;
+                    
+        }
+
+    }
+}
---
+++ SumConsecutives/SumConsecutives/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumConsecutives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> list = Data.data;
+            Operation oper = new Operation();
+            foreach(var i in oper.SumConsecutives(list))
+            {
+                Console.WriteLine(i);
+            }
+            
+           //oper.SumConsecutives(list).ForEach(i=> { Console.WriteLine(i); });
+        }
+    }
+}
